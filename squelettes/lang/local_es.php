a30615c8d3d52348c835b936081ee8862f065d54
abelass <websolutions@mychacra.net>
2013-03-12 17:13:41 +0100

    maj

---
+++ squelettes/lang/local_es.php
@@ -0,0 +1,110 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP nommé admin_lang genere le NOW()
+// langue / language = en
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+
+
+
+// A
+'a' => 'to',
+'agenda_section' => 'Agenda in which sections ?',
+'agenda_section_use' => 'Which section will be used',
+'a_partir_de' => 'from',
+'all_documents' => 'All Documents',
+'all_documents_by_name' => 'All by Organisation',
+'all_members' => 'Members alphabetically',
+'autres_fonctionnalites' => 'Otras funcionalidades',
+
+// B
+
+
+
+// C
+'contact_par' => 'Contacto por',
+'chacras' => 'Una Chacra',
+'champs_obligatoire' => 'Campo obligatorio',
+'choix_fonctionnalites' => 'Seleciona una o más funcionalidades (usa ctrl para selecionar a varias)',
+'commentaire' => 'Explicaciones detalladas',
+
+// D
+'de' => 'from',
+'detailed_view' => 'Detailed view',
+'download' => 'Download',
+
+// E
+'en_construction' => 'En construcción',
+'edit' => 'Edit this event',
+
+// F
+'fonctionnalites_basiques_spip' => 'Funcionalidades estandares de spip',
+'from' => 'from',
+
+
+// H
+
+
+
+// I
+'lieu' => 'place',
+
+
+// J
+'jusque_a' => 'to',
+
+
+// L
+
+
+
+// M
+
+
+
+// N
+'news_site' => 'Última hora',
+'no_event' => 'Currently there are no events planned',
+'nom_complet' => 'Nombre e apellido',
+'nouveautes_chacras' => 'Última hora clientes',
+
+// O
+'offre_confirmation' => 'Gracias, su solicitud ha sido enregistrada.<br/>Le enviaremos una oferta detallada por correo-e',
+
+
+// P
+'prochaine_evenement' => 'Proximos eventos',
+'portafolio_developpements' => 'desarrollos específicos',
+'portafolio_sites' => 'Sitios realisados',
+
+// R
+'reduced_view' => 'Minimized view',
+'required' => 'A<font color=red>*</font> indicates a field is required',
+
+// S
+'supprimer' => 'delete',
+'see_more' => 'See more on',
+'select_country' => 'Select a country',
+
+// T
+
+
+
+// U
+
+
+
+// V
+
+
+// W
+
+
+// Y
+'your_email' => 'Your email',
+
+);
+
+?>
